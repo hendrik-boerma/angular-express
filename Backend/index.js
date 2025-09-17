768402d28285a8b3d7195c2e2b650b5853e9cecd
Hendrik Boerma <hendrikboerma@gmail.com>
2025-09-17 15:19:50 +0200

    api and data structure backend

--- Backend/index.js
+++ Backend/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import fs from "fs";
@@ -11,13 +13,49 @@ const data = JSON.parse(fs.readFileSync(path.join(__dirname, "data.json"), "utf-
 const app = express();
 const port = 4000;
 
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:4200',
+  allowedHeaders: ['x-api-key', 'Content-Type']
+}));
 
-app.get("/api/user", (req, res) => {
+app.use(express.json());
+
+const API_KEY = process.env.API_KEY;
+
+function authenticateApiKey(req, res, next) {
+  const apiKey = req.headers["x-api-key"];
+  if (apiKey === API_KEY) {
+    next();
+  } else {
+    res.status(401).json({ error: "Unauthorized: Invalid or missing API key" });
+  }
+}
+
+app.get("/api/user", authenticateApiKey, (req, res) => {
   res.json(data.users);
 });
 
-app.get("/api/product", (req, res) => {
+app.post("/api/user", authenticateApiKey, (req, res) => {
+  
+const newUser = req.body;
+
+  if (!newUser || !newUser.id || !newUser.username || !newUser.password || !newUser.name || !newUser.role) {
+    return res.status(400).json({ error: "missing user data" });
+  }
+
+  data.users.push(newUser);
+
+  fs.writeFileSync(
+    path.join(__dirname, "data.json"),
+    JSON.stringify(data, null, 2),
+    "utf-8"
+  );
+
+  res.status(201).json({ message: "User added successfully", user: newUser });
+
+});
+
+app.get("/api/product", authenticateApiKey, (req, res) => {
   res.json(data.products);
 });
 
--- Frontend/src/app/service/data.service.ts
+++ Frontend/src/app/service/data.service.ts
@@ -1,19 +1,24 @@
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { environment } from '../../environment/environment'
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class DataService {
+  private headers = new HttpHeaders({
+    'x-api-key': environment.apiKey
+  });
+
   constructor(private http: HttpClient) {}
 
-getUsers(): Observable<any> {
-  return this.http.get('http://localhost:4000/api/user');
-}
+  getUsers(): Observable<any> {
+    return this.http.get('http://localhost:4000/api/user', { headers: this.headers });
+  }
 
-getProducts(): Observable<any> {
-  return this.http.get('http://localhost:4000/api/product');
-}
-
+  getProducts(): Observable<any> {
+    return this.http.get('http://localhost:4000/api/product', { headers: this.headers });
+  }
 }
